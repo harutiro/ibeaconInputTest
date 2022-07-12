de495d5ecafa2faa2fb1b479d809f9c050205857
harutiro <harutiro2027@outlook.jp>
2022-07-12 11:31:18 +0900

    update:パーミッションの確認部分の作成 Android12に対応させた

--- app/src/main/java/io/github/com/harutiro/ibeaconinputtest/MainActivity.kt
+++ app/src/main/java/io/github/com/harutiro/ibeaconinputtest/MainActivity.kt
@@ -1,5 +1,6 @@
 package io.github.com.harutiro.ibeaconinputtest
 
+import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +11,7 @@ import org.altbeacon.beacon.Beacon
 import org.altbeacon.beacon.BeaconManager
 import org.altbeacon.beacon.BeaconParser
 import org.altbeacon.beacon.Region
+import pub.devrel.easypermissions.EasyPermissions
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,8 +19,24 @@ class MainActivity : AppCompatActivity() {
     private val TAG: String = "HogeActivity"
     // iBeaconのデータを認識するためのParserフォーマット
     val IBEACON_FORMAT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"
-    //パーミッション確認ようのコード
-    private val PERMISSION_REQUEST_COARSE_LOCATION = 1
+    //パーミッション確認用のコード
+    private val PERMISSION_REQUEST_CODE = 1
+
+    //どのパーミッションを許可したいかリスト化する
+    val permissions = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S){
+        arrayOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.BLUETOOTH_CONNECT,
+            Manifest.permission.BLUETOOTH_SCAN,
+            Manifest.permission.BLUETOOTH_ADVERTISE
+        )
+    }else{
+        arrayOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+        )
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,23 +45,27 @@ class MainActivity : AppCompatActivity() {
 
         //パーミッション確認
         //TODO:ロケーションの取得の常に許可をできるようにする
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (this.checkSelfPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION),PERMISSION_REQUEST_COARSE_LOCATION);
-            }
+        if (!EasyPermissions.hasPermissions(this, *permissions)) {
+            // パーミッションが許可されていない時の処理
+            EasyPermissions.requestPermissions(this, "パーミッションに関する説明", PERMISSION_REQUEST_CODE, *permissions)
         }
 
-        //絞り込みをする部分
-        //今回nullなので、全てを取得する。
-        //id1:uuid id2:major id3:minor
-        val mRegion = Region("unique-id-001", null, null, null)
+        //パーミッションが許可された時にIbeaconが動く
+        if(EasyPermissions.hasPermissions(this, *permissions)){
+            //絞り込みをする部分
+            //今回nullなので、全てを取得する。
+            //id1:uuid id2:major id3:minor
+            val mRegion = Region("unique-id-001", null, null, null)
 
-        val beaconManager =  BeaconManager.getInstanceForApplication(this)
-        // Set up a Live Data observer so this Activity can get ranging callbacks
-        // observer will be called each time the monitored regionState changes (inside vs. outside region)
-        beaconManager.getRegionViewModel(mRegion).rangedBeacons.observe(this, rangingObserver)
-        beaconManager.beaconParsers.add(BeaconParser().setBeaconLayout(IBEACON_FORMAT))
-        beaconManager.startRangingBeacons(mRegion)
+            val beaconManager =  BeaconManager.getInstanceForApplication(this)
+            // Set up a Live Data observer so this Activity can get ranging callbacks
+            // observer will be called each time the monitored regionState changes (inside vs. outside region)
+            beaconManager.getRegionViewModel(mRegion).rangedBeacons.observe(this, rangingObserver)
+            beaconManager.beaconParsers.add(BeaconParser().setBeaconLayout(IBEACON_FORMAT))
+            beaconManager.startRangingBeacons(mRegion)
+        }
+
+
     }
 
     //取得した時の動作部分
